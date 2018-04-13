32f42dccf14989fee3cf7cb4b61ae366ced22548
ismurray <ians.murray@yahoo.com>
2018-04-13 15:57:45 -0400

    Add destroy item view&functionality
    - Add delete button to single item view
    - Add deleteItem action to item route, which destroys the currently
    viewed item, then transitions to the index

--- app/routes/items/item.js
+++ app/routes/items/item.js
@@ -4,5 +4,13 @@ export default Route.extend({
   model: function (params) {
     const id = params.item_id
     return this.get('store').findRecord('item', id)
+  },
+  actions: {
+    deleteItem (item) {
+      console.log('delete', item.get('name'))
+      item.destroyRecord()
+        .then(() => this.transitionTo('items'))
+        .catch(console.error)
+    }
   }
 })
