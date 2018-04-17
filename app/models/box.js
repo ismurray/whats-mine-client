cf494ba04a42bbc0ab000d810f569c48f1c9220a
ismurray <ians.murray@yahoo.com>
2018-04-16 21:12:00 -0400

    Add Editable property to Boxes
    - Add editable property to Box model, which returns the write access for
    the current user on any given Box.
    - Edit the Box view and Box edit view templates to only show editing
    options if editable = true

--- app/models/box.js
+++ app/models/box.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   items: DS.hasMany('item'),
   users: DS.hasMany('user'),
-  permissions: DS.hasMany('users-box', { inverse: 'box' })
+  permissions: DS.hasMany('users-box', { inverse: 'box' }),
+  editable: DS.attr()
 })
