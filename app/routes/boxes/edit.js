73ec5e29d3bb62d6210d8f750da4aa13967f54d1
ismurray <ians.murray@yahoo.com>
2018-04-19 16:17:06 -0400

    Add Success/Failure Toasts to Edit Box View
    Add Success/Failure Toasts to Edit Box View

--- app/routes/boxes/edit.js
+++ app/routes/boxes/edit.js
@@ -12,24 +12,38 @@ export default Route.extend({
   actions: {
     updateBox (box) {
       box.save()
+        .then((response) => {
+          this.toast.success('Box Name Saved', 'Success', {preventDuplicates: false})
+          return response
+        })
         .then((box) => this.transitionTo('/boxes/' + box.get('id')))
-    },
-    createUsersBox (usersBoxPojo) {
-      console.log('in the route babyyy', usersBoxPojo)
-      const usersBox = this.get('store').createRecord('users-box', usersBoxPojo)
-      usersBox.save()
-        .then((usersBox) => this.transitionTo('/boxes/' + usersBox.get('box_id')))
+        .catch(() => this.toast.error('Error Renaming the Box', 'Failure', {preventDuplicates: false}))
     },
     updatePermission (permission) {
       permission.save()
+        .then((response) => {
+          this.toast.success('Box Permission Saved', 'Success', {preventDuplicates: false})
+          return response
+        })
+        .catch(() => this.toast.error('Error Saving Box Permission', 'Failure', {preventDuplicates: false}))
     },
     deletePermission (permission) {
       permission.destroyRecord()
+        .then((response) => {
+          this.toast.success('Box Permission Deleted', 'Success', {preventDuplicates: false})
+          return response
+        })
+        .catch(() => this.toast.error('Error Deleting Box Permission', 'Failure', {preventDuplicates: false}))
     },
     createPermission (usersBoxPojo) {
       const boxId = usersBoxPojo.box_id
       return this.get('auth').createPermission(usersBoxPojo)
               .then(() => this.store.findRecord('box', boxId, { reload: true }))
+              .then((response) => {
+                this.toast.success('User given access to this Box', 'Success', {preventDuplicates: false})
+                return response
+              })
+              .catch(() => this.toast.error('Error Sharing Box with User', 'Failure', {preventDuplicates: false}))
     }
   }
 })
