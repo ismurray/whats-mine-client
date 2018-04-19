b0e31917075f685013c426afc490b8213bbc90ee
ismurray <ians.murray@yahoo.com>
2018-04-19 16:22:42 -0400

    Add Success/Fail Toasts to New Box view
    Add Success/Fail Toasts to New Box view

--- app/routes/boxes/new.js
+++ app/routes/boxes/new.js
@@ -6,8 +6,13 @@ export default Route.extend({
       console.log('in the route babyyy')
       const box = this.get('store').createRecord('box', boxPojo)
       box.save()
+        .then((response) => {
+          this.toast.success('Box Created', 'Success', {preventDuplicates: false})
+          return response
+        })
         // transition to the box view of the newly created box
         .then((box) => this.transitionTo('/boxes/' + box.get('id')))
+        .catch(() => this.toast.error('Error Creating this Box', 'Failure', {preventDuplicates: false}))
     }
   }
 })
